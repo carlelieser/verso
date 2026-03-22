fee6019c1aaa7faec686e34ac2e52b92c46abb72
Carlos Santos <carloseliesersantos@gmail.com>
2026-03-22 05:25:59 -0400

    refactor: extract shared bottom sheet animation config

---
+++ src/constants/animation.ts
@@ -0,0 +1,7 @@
+/**
+ * Empty animation config object that overrides gorhom/bottom-sheet's default
+ * spring animation to match HeroUI Native's smoother spring on Android.
+ *
+ * @see feedback_bottomsheet_animation.md
+ */
+export const BOTTOM_SHEET_ANIMATION_CONFIG = {};
